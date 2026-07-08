ab292849549e44115e8b9cb4d6c57362a2392f08
PuneetSivananda <puneet.sivananda@gmail.com>
2026-07-09 00:32:05 +1000

    feat: updates to the template added the stats section

---
+++ solar-platform-ui/src/lib/components/ui/KpiCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let {
+		value,
+		label,
+		children
+	}: {
+		value?: string;
+		label?: string;
+		children?: () => any;
+	} = $props();
+</script>
+
+<div class="text-center">
+    <h3 class="text-7xl font-bold text-primary">{value}</h3>
+    <h6>{label}</h6>
+</div>
--- solar-platform-ui/src/routes/+page.svelte
+++ solar-platform-ui/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import HeroSlider from '$lib/sections/Slider.svelte';
+	import KPISection from '$lib/sections/kpi.svelte'
 </script>
 
 <HeroSlider />
+<KPISection />
